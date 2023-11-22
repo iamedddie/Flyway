b32cef8d11cac2201b6fd7f10c619a96b70260dc
iamedddie <edyolisa@gmail.com>
2023-11-22 10:09:02 +0000

    Start up

---
+++ test/java/com/barclay/FlywayDemo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclay.FlywayDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
